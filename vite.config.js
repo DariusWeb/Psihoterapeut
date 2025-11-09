2b45b35dc47432cc5de82bbfbb739376107ec97f
DariusWeb <41270752+DariusWeb@users.noreply.github.com>
2025-11-09 15:09:33 +0200

    Add GitHub Pages deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -24,4 +23,5 @@ export default defineConfig({
   //     }
   //   }
   // }
+  base: '/Psihoterapeut/', // IMPORTANT for GitHub Pages
 })
